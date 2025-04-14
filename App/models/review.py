a4a82d496f606fc38db00fdc5c39ba0535d1ee28
calebgh <calebc386@gmail.com>
2025-04-14 15:07:45 -0400

    Create review.py

---
+++ App/models/review.py
@@ -0,0 +1,18 @@
+from App.database import db
+from datetime import datetime
+
+
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.Text)
+    tenant_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    listing_id = db.Column(db.Integer, db.ForeignKey('listing.id'))
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+def __init__(self, rating, comment, tenant_id, listing_id):
+    self.rating = rating
+    self.comment = comment
+    self.tenant_id = tenant_id
+    self.listing_id = listing_id
