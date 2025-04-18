76d030c870f676566c89d006aae96725de2b826f
calebgh <calebc386@gmail.com>
2025-04-18 13:55:07 -0400

    Update review.py

--- App/models/review.py
+++ App/models/review.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rating = db.Column(db.Integer, nullable=False)
-    comment = db.Column(db.Text)
+    comment = db.Column(db.String(200))
     tenant_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     listing_id = db.Column(db.Integer, db.ForeignKey('listing.id'))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
