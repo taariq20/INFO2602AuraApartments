1811255b9ad0097205b5755482bf5083ebcbdbfa
taariq20 <taariqboy20@gmail.com>
2025-04-19 19:14:42 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -3,7 +3,7 @@ from App.database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username =  db.Column(db.String(20), nullable=False, unique=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(120), nullable=False)
     type = db.Column(db.String(50))  
 
@@ -17,7 +17,7 @@ class User(db.Model):
         self.set_password(password)
 
     def get_json(self):
-        return{
+        return {
             'id': self.id,
             'username': self.username
         }
@@ -29,22 +29,23 @@ class User(db.Model):
     def check_password(self, password):
         """Check hashed password."""
         return check_password_hash(self.password, password)
-        
+
+
 class Tenant(User):
     __tablename__ = 'tenant'
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     reviews = db.relationship('Review', backref='tenant', lazy=True)
+
     __mapper_args__ = {
         'polymorphic_identity': 'tenant',
     }
+
 
 class Landlord(User):
     __tablename__ = 'landlord'
     id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
     listings = db.relationship('Listing', backref='landlord', lazy=True)
+
     __mapper_args__ = {
         'polymorphic_identity': 'landlord',
     }
-
-from App.models.models import Review
-Tenant.reviews = db.relationship('Review', backref='tenant', lazy=True)
