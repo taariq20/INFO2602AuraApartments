0850b1b89f6de134c4ad8f2699971d21180c70d6
taariq20 <taariqboy20@gmail.com>
2025-04-20 23:59:16 -0400

    Update initialize.py

--- App/controllers/initialize.py
+++ App/controllers/initialize.py
@@ -5,7 +5,6 @@ from App.models.user import Landlord
 def initialize():
     db.drop_all()
     db.create_all()
-
-    create_user('bob', 'bobpass')
-
+    
     create_user('landlord1', 'landlordpass')
+    create_user('bob', 'bobpass')
